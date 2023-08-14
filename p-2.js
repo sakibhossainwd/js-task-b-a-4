7d1c8bf38727f201275c1156facbeab8c3fb28bd
sakibhossain <sakibhossainceo@gmail.com>
2023-08-14 10:11:11 -0700

    p-2 another way solve

--- p-2.js
+++ p-2.js
@@ -1,5 +1,5 @@
 // problem-02
-function matchFinder(string1, string2) {
+function matchFinder01(string1, string2) {
     // is perameter string
     if(typeof string1 !== 'string' || typeof string2 !== 'string'){
         return 'Please make sure two perameter is string'
@@ -16,7 +16,38 @@ function matchFinder(string1, string2) {
 
 const stringOne = 'Sakib Hossain';
 const stringTwo = 'Hossain';
-const result = matchFinder(stringOne, stringTwo);
+const result = matchFinder01(stringOne, stringTwo);
 // console.log(result);
 
+function matchFinder02(string01, string02) {
+    // is perameter string
+    if(typeof string01 !== 'string' || typeof string02 !== 'string'){
+        return 'Please make sure two perameter is string'
+    }
+
+    // check the match string
+    const test1 = string01.length;
+    const test2 = string02.length;
+    const maxIndex = test1 - test2;
+    console.log(maxIndex);
+    for (let i = 0; i <= maxIndex; i++) {
+        let matched = true;
+        for (let j = 0; j < test2; j++) {
+            if (string01[i + j] !== string02[j]) {
+                matched = false;
+                break;
+            }
+        }
+        if (matched) {
+            return true;
+        }
+    }
+    
+    return false;
+}
+
+
+console.log(matchFinder02("Sakib Hossain", "Hossain"));  
+// console.log(matchFinder02("abcdef", "xyz"));
+// console.log(matchFinder02("abcdef", 123));
 
